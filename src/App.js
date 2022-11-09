83e448b266921909f71e1649519ee32b4a847284
Felix Odete <felix.odette@gmail.com>
2022-11-09 08:24:28 +0300

    gitflow-feature-stash: landing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import AddUser from './AddUser';
+import UserList from './UserList';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const createUser = (user) => {
+    user.numGamesPlayed = 0;
+    setUsers([...users, user]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className={"App-title"}>ReactND - Coding Practice</h1>
       </header>
     </div>
   );
